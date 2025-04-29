7e564b0e744b99158eef8b1c2b8bc1cfb40e6353
Yeison Serrano <ydanielsb97@gmail.com>
2025-04-29 07:25:56 -0400

    Adding phone tap action

--- Scenes/Plane/Plane.gd
+++ Scenes/Plane/Plane.gd
@@ -9,7 +9,7 @@ var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
 @onready var engine_sound: AudioStreamPlayer = $EngineSound
 
 const JUMP_SPEED: float = -350.0 
-
+var tap_pressed = false
 #signal plane_died
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,9 +23,14 @@ func fly(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += _gravity * delta
 	
-	if Input.is_action_just_pressed("ui_accept"):
+	if tap_pressed or Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_SPEED
 		animation_player.play("jump")
+		tap_pressed = false
+
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		tap_pressed = true
 
 func die() -> void:
 	animated_sprite_2d.stop()
